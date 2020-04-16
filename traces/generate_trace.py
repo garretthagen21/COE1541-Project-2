be2742639caf365319753e83ad00061ac3591f11
Garrett Hagen <gbh8@pitt.edu>
2020-04-16 18:47:59 -0400

    added custom base for instruction gen

--- traces/generate_trace.py
+++ traces/generate_trace.py
@@ -2,12 +2,34 @@ import argparse
 import random
 
 
+
+
+def convert_to_base(decimal_number, base,addr_size):
+    DIGITS = '0123456789abcdef'
+    remainder_stack = []
+
+    while decimal_number > 0:
+        remainder = decimal_number % base
+        remainder_stack.append(remainder)
+        decimal_number = decimal_number // base
+
+    new_digits = []
+    while remainder_stack:
+        new_digits.append(DIGITS[remainder_stack.pop()])
+
+    # Hack to make binary numbers addr_size bits
+    if base == 2:
+        while len(new_digits) < addr_size:
+            new_digits.insert(0,'0')
+
+
+    return ''.join(new_digits)
 def coin_flip(prob=0.5):
     prob = min(1.0, max(0.0, prob))
     return random.randint(1, 100) <= prob * 100
 
 
-def create_trace_file(filename, addr_size, num_accesses, reuse_rate):
+def create_trace_file(filename, addr_size, num_accesses, reuse_rate,number_base):
     wFile = open(filename, 'w+')
     maxAddr = 2 ** addr_size - 1
     time = 0
@@ -23,6 +45,9 @@ def create_trace_file(filename, addr_size, num_accesses, reuse_rate):
 
         # Get the access mode
         mode = 'w' if coin_flip() else 'r'
+
+        # Convert to appropriate number system
+        new_addr = convert_to_base(new_addr,number_base,addr_size)
 
         # Write to the file
         wFile.write(str(mode)+" "+str(new_addr)+" "+str(time)+"\n")
@@ -41,6 +66,7 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--addr-size', dest='addr_size', default=32, type=int, help="Address size in bits")
     parser.add_argument('-a', '--num-accesses', dest='num_accesses', default=100, type=int,
                         help="How many accesses we want")
+    parser.add_argument('-b', '--base',dest='number_base',default=10,type=int,help="Number base (e.g. decimal = 10, binary = 2, hex = 16)")
     parser.add_argument('-r', '--reuse-rate', dest='reuse_rate', default=0.3, type=float,
                         help="How often we want to reuse an existing address, This will increase hit rate")
 
@@ -48,6 +74,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Verify they are correct
-    create_trace_file(args.file_name, args.addr_size, args.num_accesses, args.reuse_rate)
+    create_trace_file(args.file_name, args.addr_size, args.num_accesses, args.reuse_rate,args.number_base)
 
     print("Done!")
